adf766eb6f8c5ded0874c25f2e9df399172f03dc
MAgungPratama21 <2300018005@webmail.uad.ac.id>
2024-05-20 15:33:23 +0700

    update css,js, dan html

--- js/script.js
+++ js/script.js
@@ -1,12 +1,24 @@
-const navbarNav = document.querySelector(".navbar-nav");
-const menu = document.querySelector("#menu");
+document.addEventListener("DOMContentLoaded", () => {
+  feather.replace();
 
-menu.onclick = () => {
-  navbarNav.classList.toggle("active");
-};
+  const navbarNav = document.querySelector(".navbar-nav");
+  const menu = document.querySelector("#menu");
 
-document.addEventListener("click", function (e) {
-  if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
-    navbarNav.classList.remove("active");
-  }
+  menu.onclick = () => {
+    navbarNav.classList.toggle("active");
+  };
+
+  document.addEventListener("click", function (e) {
+    if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
+      navbarNav.classList.remove("active");
+    }
+  });
+
+  const searchIcon = document.getElementById("search");
+  const searchBox = document.getElementById("search-box");
+
+  searchIcon.addEventListener("click", (e) => {
+    e.preventDefault();
+    searchBox.classList.toggle("active");
+  });
 });
