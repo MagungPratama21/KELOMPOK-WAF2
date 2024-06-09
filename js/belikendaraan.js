2d474bb8c31de354eaf06bd315830c89fc549903
OnlyOne855 <2300018003@webmail.uad.ac.id>
2024-06-09 15:00:19 +0700

    update belikendaraan

--- js/belikendaraan.js
+++ js/belikendaraan.js
@@ -5,20 +5,32 @@ document.addEventListener("DOMContentLoaded", () => {
   const menu = document.querySelector("#menu");
 
   menu.onclick = () => {
-    navbarNav.classList.toggle("active");
+      navbarNav.classList.toggle("active");
   };
 
   document.addEventListener("click", function (e) {
-    if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
-      navbarNav.classList.remove("active");
-    }
+      if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
+          navbarNav.classList.remove("active");
+      }
   });
 
   const searchIcon = document.getElementById("search");
   const searchBox = document.getElementById("search-box");
 
   searchIcon.addEventListener("click", (e) => {
-    e.preventDefault();
-    searchBox.classList.toggle("active");
+      e.preventDefault();
+      searchBox.classList.toggle("active");
+  });
+
+  const carousel = document.getElementById("carousel");
+  const nextBtn = document.getElementById("nextBtn");
+  const prevBtn = document.getElementById("prevBtn");
+
+  nextBtn.addEventListener("click", () => {
+      carousel.scrollBy({ left: 400, behavior: 'smooth' });
+  });
+
+  prevBtn.addEventListener("click", () => {
+      carousel.scrollBy({ left: -400, behavior: 'smooth' });
   });
 });
