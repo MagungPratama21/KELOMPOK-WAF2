0aaeeccce62b0199c9ac764cc1c1cba692c5195d
MAgungPratama21 <2300018005@webmail.uad.ac.id>
2024-06-19 21:08:29 +0700

    update menjadi php

---
+++ header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>WAF GARAGE 21</title>
+    <!-- Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900&display=swap" rel="stylesheet"/>
+
+    <!-- icon -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.28.0/feather.min.js"></script>
+    <link rel="stylesheet" href="css2/style.css" />
+</head>
+<body>
+    <div class="navbar">
+        <a href="#" class="navbar-logo">WAF21<span>GARAGE.com</span></a>
+
+        <div class="navbar-nav">
+            <a href="#home">Home</a>
+            <a href="#mobil">Best Seller </a>
+            <a href="#spare-part">Spare Part</a>
+            <a href="#bantuan">Bantuan</a>
+            <a href="belikendaraan.html">Mobil Baru</a>
+            <a href="jualkendaraan.html">Mobil Bekas</a>
+        </div>
+
+        <div class="navbar-tambahan">
+            <a href="#" id="search"><i data-feather="search"></i></a>
+            <a href="login.html" id="login"><i data-feather="log-in"></i></a>
+            <a href="#" id="user" style="display: none;"><i data-feather="user"></i></a>
+            <a href="#" id="menu"><i data-feather="menu"></i></a>
+        </div>
+
+        <div class="search-box" id="search-box">
+            <input type="text" name="search" id="search-input" placeholder="cari kendaraan..." />
+        </div>
+    </div>
---
+++ index.php
@@ -0,0 +1,181 @@
+<?php
+include 'header.php';
+?>
+
+<section class="home" id="home">
+    <video autoplay muted loop id="video-background">
+        <source src="video/gtr.mp4" type="video/mp4">
+    </video>
+    <div class="home-text">
+        <h1>MAU MOBIL ELIT DANA SULIT? <span><br>WAF 21 GARAGE SOLUSINYA</span></h1>
+        <p>Kami Menyediakan Berbagai Macam Jenis <br> Type Kendaraaan di Seluruh Dunia</p>
+        <a href="jualkendaraan.html" class="btn" id="mobil-baru-btn">MOBIL BARU</a>
+        <a href="belikendaraan.html" class="btn" id="mobil-bekas-btn">MOBIL BEKAS</a>
+    </div>
+</section>
+
+
+    <section class="mobil" id="mobil">
+        <div class="header-mobil">
+            <span>Semua Jenis Mobil di WAF21GARAGE</span>
+            <h2>Kami Punya Semua Tipe Mobil Sesuai Keinginan Anda</h2>
+            <p>Berikut Merupakan Best Seller Product di Tempat Kami</p>
+        </div>
+     <div class="content">
+    <div class="box">
+        <a href="porschecayman.html">
+            <img src="foto2/foto1.jpg">
+            <h2>Porsche Cayman 718</h2>
+        </a>
+    </div>
+    <div class="box">
+        <a href="toyotagt.html">
+            <img src="foto2/foto2.jpg">
+            <h2>Toyota GT 86</h2>
+        </a>
+    </div>
+    <div class="box">
+        <a href="ferrari.html">
+            <img src="foto2/foto3.jpg">
+            <h2>Ferrari</h2>
+        </a>
+    </div>
+    <div class="box">
+        <a href="mclaren.html">
+            <img src="foto2/foto4.jpg">
+            <h2>Mclaren 520s</h2>
+        </a>
+    </div>
+    <div class="box">
+        <a href="nissangtr.html">
+            <img src="foto2/foto5.jpg">
+            <h2>Nissan GTR R-35</h2>
+        </a>
+    </div>
+    <div class="box">
+        <a href="hondansx.html">
+            <img src="foto2/foto6.jpg">
+            <h2>Honda NSX</h2>
+        </a>
+    </div>
+    <div class="box">
+        <a href="mercedesamg.html">
+            <img src="foto2/foto7.jpg">
+            <h2>Marcedez AMG</h2>
+        </a>
+    </div>
+    <div class="box">
+        <a href="hondacivic.html">
+            <img src="foto2/foto8.jpg">
+            <h2>Honda Civic</h2>
+        </a>
+    </div>
+    <div class="box">
+        <a href="koeniggsegg.html">
+            <img src="foto2/foto9.jpg">
+            <h2>Koeniggsegg</h2>
+        </a>
+    </div>
+    <div class="box">
+        <a href="bugatti.html">
+            <img src="foto2/foto10.jpg">
+            <h2>Bugatti</h2>
+        </a>
+    </div>
+</div>
+
+    </section>
+
+    <section class="spare-part" id="spare-part">
+        <div class="header">
+            <span>KAMI JUGA MENJUAL BEBERAPA SPARE PART</span>
+            <h2>Spare Part Kami Terbuat Dari Bahan Chrome dan Titanium</h2>
+            <p>Berikut Merupakan Best Seller Product di Tempat Kami</p>
+        </div>
+        <div class="content">
+            <div class="box">
+                <img src="foto2/part1.png">
+                <h3>Ban Mobil</h3>
+                <div class="bagi">
+                    <a href="#" class="button">Beli</a>
+                    <a href="#" class="details">More detail</a>
+                </div>
+            </div>
+            <div class="box">
+                <img src="foto2/part2.png">
+                <h3>Velg Mobil</h3>
+                <div class="bagi">
+                    <a href="#" class="button">Beli</a>
+                    <a href="#" class="details">More detail</a>
+                </div>
+            </div>
+            <div class="box">
+                <img src="foto2/part3.png">
+                <h3>Busi Mobil</h3>
+                <div class="bagi">
+                    <a href="#" class="button">Beli</a>
+                    <a href="#" class="details">More detail</a>
+                </div>
+            </div>
+            <div class="box">
+                <img src="foto2/part4.png">
+                <h3>Cakram Mobil</h3>
+                <div class="bagi">
+                    <a href="#" class="button">Beli</a>
+                    <a href="#" class="details">More detail</a>
+                </div>
+            </div>
+            <div class="box">
+                <img src="foto2/part5.png">
+                <h3>Aki Mobil</h3>
+                <div class="bagi">
+                    <a href="#" class="button">Beli</a>
+                    <a href="#" class="details">More detail</a>
+                </div>
+            </div>
+            <div class="box">
+                <img src="foto2/part6.png">
+                <h3>Disk Brake</h3>
+                <div class="bagi">
+                    <a href="#" class="button">Beli</a>
+                    <a href="#" class="details">More detail</a>
+                </div>
+            </div>
+            <div class="box">
+                <img src="foto2/part7.png">
+                <h3>Spoiler</h3>
+                <div class="bagi">
+                    <a href="#" class="button">Beli</a>
+                    <a href="#" class="details">More detail</a>
+                </div>
+            </div>
+            <div class="box">
+                <img src="foto2/part8.png">
+                <h3>Bodykit</h3>
+                <div class="bagi">
+                    <a href="#" class="button">Beli</a>
+                    <a href="#" class="details">More detail</a>
+                </div>
+            </div>
+            <div class="box">
+              <img src="foto2/part9.png">
+              <h3>Velg Mobil</h3>
+              <div class="bagi">
+                  <a href="#" class="button">Beli</a>
+                  <a href="#" class="details">More detail</a>
+              </div>
+          </div>
+          <div class="box">
+            <img src="foto2/part10.png">
+            <h3>Velg Mobil</h3>
+            <div class="bagi">
+                <a href="#" class="button">Beli</a>
+                <a href="#" class="details">More detail</a>
+            </div>
+        </div>
+        </div>
+    </section>
+
+<?php
+include 'footer.php';
+?>
